70a015ca594adfb5a7b2a99bff08a337ffdab4ab
lillybo <lillian.bojkov@gmail.com>
2023-08-03 14:10:23 +0300

    GDB-57 adding player movement

---
+++ Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float dodgeSpeed;
+    float xInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(xInput * dodgeSpeed * Time.deltaTime, 0, 0);
+
+    }
+}
