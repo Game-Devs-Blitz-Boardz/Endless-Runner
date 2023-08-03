be02696b478f0bc872a82c033a862bc8f673d7e3
lillybo <lillian.bojkov@gmail.com>
2023-08-03 14:39:34 +0300

    GDB-57 adding enemy spawning

---
+++ GameManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    public float maxSpaqwnPointX = 1.5f;
+    public float spawnRate = 0.8f;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies() {
+        while (true) {
+            Spawn();
+
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+
+    public void Spawn() {
+
+        float randomSpawnX = Random.Range(-maxSpaqwnPointX, maxSpaqwnPointX);
+
+        Vector3 enemySpawnPos = spawnPoint.position;
+        enemySpawnPos.x = randomSpawnX;
+
+        Instantiate(enemyPrefab, enemySpawnPos, Quaternion.identity);
+    }
+}
